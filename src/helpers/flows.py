5751c3aa0778ccae549549e1d091952a012ef75b
Thomas Lemaire <thomas.lemaire@sendoc.fr>
2025-11-19 15:11:23 +0100

    dpi 512

--- src/helpers/flows.py
+++ src/helpers/flows.py
@@ -147,7 +147,7 @@ def node_sardine(config, *, base64=None, debug=False):
         img_b64=base64,
         device="cpu",
         conf=.5,
-        pdf_dpi=768
+        pdf_dpi=512
     )
 
     print_debug(f"[SARDINE] Classified as: {cls}", debug)
